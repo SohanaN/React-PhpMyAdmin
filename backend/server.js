6f64c6bb10a6fa2027c32f2065313bc43f9dabca
SohanaN <sohananaz01@gmail.com>
2023-03-28 01:22:02 +0600

    create & upadate

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors=require("cors");
 const mysql = require('mysql');
+
 const app = express();
+app.use(express.json());
 
 app.use(cors());
+
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -11,10 +14,39 @@ const db = mysql.createConnection({
     database: "react-phpmyadmin"
 })
 
+// show 
 app.get("/",(req, res)=>{
     const sql = "SELECT * FROM student";
     db.query(sql,(err,data)=>{
         if(err) return app.json("Error");
+        return res.json(data);
+    })
+})
+
+// create 
+app.post('/create',(req,res)=>{
+    const sql="INSERT INTO student (`Name`, `Email`) VALUES (?)";
+    const values = [
+        req.body.name,
+        req.body.email
+    ]
+    db.query(sql, [values], (err, res)=>{
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+})
+
+// update 
+app.put('/update/:id',(req,res)=>{
+    const sql="update student set `Name` = ?, `Email` = ? where ID = ?";
+    const values = [
+        req.body.name,
+        req.body.email
+    ]
+    const id = req.params.id;
+
+    db.query(sql, [...values, id], (err, data)=>{
+        if(err) return res.json("Error");
         return res.json(data);
     })
 })
